0a909f335e6fdff6392c7ce59af35cead73fd1d1
ezechieldef <ezechielahouandjinou@gmail.com>
2024-05-15 16:21:10 +0100

    ...

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,22 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Auth::routes();
-
+Route::get('theme-toggle', function () {
+    if (session('theme')) {
+        session()->forget('theme');
+    } else {
+        session(['theme' => 'dark']);
+    }
+    return back();
+})->name('theme-toggle');
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::redirect('/home', '/');
 Route::resource("users", UserController::class);
