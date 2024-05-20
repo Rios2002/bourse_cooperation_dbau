133ce766ba9dd33c9ef9d350da9ea88aaa3cd4b0
ezechieldef <ezechielahouandjinou@gmail.com>
2024-05-20 09:25:52 +0100

    en un bloc

--- resources/views/bourse-disponible/home.blade.php
+++ resources/views/bourse-disponible/home.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     <div class="row">
         @foreach ($bourses as $brs)
-            <div class="col-lg-4">
+            <div class="col-lg-4 col-sm-6">
                 <div class="card">
                     <div class="card-body">
                         <div class="text-center">
