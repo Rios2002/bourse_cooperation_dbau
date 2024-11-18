28957bb0a719bb8d1ec678d775e1c047ec44e40f
ezechieldef <ezechielahouandjinou@gmail.com>
2024-11-18 08:42:29 +0100

    ...

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 @php
     // dd(request()->session()->get('theme', null));
     // dd(request()->cookie('theme'));
-    request()->headers->set('Host', env('APP_URL'));
+    request()->headers->set('Host', 'localhost');
     $dark_Theme = request()->session()->get('theme', null);
 
 @endphp
