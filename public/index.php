d7f7f21ffdc7eb495093392f30905d5671abbc23
ezechieldef <ezechielahouandjinou@gmail.com>
2024-11-18 09:16:11 +0100

    ...

--- public/index.php
+++ public/index.php
@@ -24,8 +24,8 @@ function getEnvVariable($key, $default = null)
 if (getEnvVariable('APP_ENV') == "production") {
     $app_url = getEnvVariable("APP_URL");
     $_SERVER['HTTP_HOST'] = $app_url;
-    $_SERVER['HOST'] = $app_url;
-    $_SERVER['Host'] = $app_url;
+    // $_SERVER['HOST'] = $app_url;
+    // $_SERVER['Host'] = $app_url;
 }
 
 define('LARAVEL_START', microtime(true));
